ab190eb46c1f5bb4e97685814b63e29596bc8e09
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-27 16:59:39 +0800

    生成无用的yolov3

--- main.py
+++ main.py
@@ -1,62 +1,31 @@
-'''
-Author: yuanshuailk 449235430@qq.com
-Date: 2024-05-27 15:11:44
-LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-27 15:30:35
-FilePath: /wangwei/main.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-
 import cv2
 import numpy as np
 
-# 加载 YOLO
-net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-layer_names = net.getLayerNames()
-output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
 # 加载图像
-img = cv2.imread("dog.jpg")
-img = cv2.resize(img, None, fx=0.4, fy=0.4)
-height, width, channels = img.shape
+img = cv2.imread('image.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# 检测对象
-blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
-net.setInput(blob)
-outs = net.forward(output_layers)
+# Canny 边缘检测
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-# 显示信息在屏幕上
-class_ids = []
-confidences = []
-boxes = []
-for out in outs:
-    for detection in out:
-        scores = detection[5:]
-        class_id = np.argmax(scores)
-        confidence = scores[class_id]
-        if confidence > 0.5:
-            # 对象检测
-            center_x = int(detection[0] * width)
-            center_y = int(detection[1] * height)
-            w = int(detection[2] * width)
-            h = int(detection[3] * height)
-            # 矩形坐标
-            x = int(center_x - w / 2)
-            y = int(center_y - h / 2)
-            boxes.append([x, y, w, h])
-            confidences.append(float(confidence))
-            class_ids.append(class_id)
+# 霍夫线变换
+lines = cv2.HoughLines(edges, 1, np.pi/180, 200)
 
-indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+# 在图像上绘制线
+for line in lines:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
 
-for i in range(len(boxes)):
-    if i in indexes:
-        x, y, w, h = boxes[i]
-        label = str(classes[class_ids[i]])
-        if label == 'dog':
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img, label, (x, y + 30), font, 2, (0, 255, 0), 3)
+    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-cv2.imshow("Image", img)
+# 显示图像
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
