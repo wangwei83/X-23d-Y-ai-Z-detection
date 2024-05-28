ac2143d9f3b2ae7666ece8b4b1d2d4f5b12fb5f8
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-28 23:38:09 +0800

    增加remove_plane等实现

--- M3DM-from-Scratch/utils/mvtec3d_util.py
+++ M3DM-from-Scratch/utils/mvtec3d_util.py
@@ -3,12 +3,20 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-28 11:05:44
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-28 11:06:42
+LastEditTime: 2024-05-28 23:27:38
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/mvtec3d_util.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 import tifffile as tiff
+
+def organized_pc_to_unorganized_pc(organized_pc):
+    '''
+    Convert organized point cloud to unorganized point cloud
+    '''
+    # 将organized_pc数组重塑为一个二维数组，新数组的第一维的大小是原数组的第一维和第二维的元素总数，第二维的大小是原数组的第三维的大小。
+    # 这种操作通常用于将一个多维数组“展平”为一个二维数组，例如在机器学习中，我们可能需要将一个三维的图像数据集展平为二维，以便可以将其输入到一个机器学习模型中。
+    return organized_pc.reshape(organized_pc.shape[0] * organized_pc.shape[1], organized_pc.shape[2])
 
 def read_tiff_organized_pc(tiff_path):
     '''
--- M3DM-from-Scratch/utils/preprocessing.py
+++ M3DM-from-Scratch/utils/preprocessing.py
@@ -2,28 +2,42 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-28 10:48:48
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-28 22:14:24
+LastEditTime: 2024-05-28 23:12:57
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/preprocessing.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 import argparse
+import os
 from pathlib import Path
 from PIL import Image
 import numpy as np
 import mvtec3d_util as mvt_util
 
+def remove_plane(organized_pc_clean, organized_rgb,distance_threshold=0.005):
+    # Remove the plane
+    
+    unorganized_pc  = mvt_util.organized_pc_to_unorganized_pc(organized_pc_clean)
+    
+    
+    planeless_organized_pc = organized_pc_clean
+    planeless_organized_rgb = organized_rgb
+    return planeless_organized_pc, planeless_organized_rgb
+
 def preprocess_pc(tiff_path):
     organized_pc = mvt_util.read_tiff_organized_pc(tiff_path)
-    # print(f"Preprocessing {tiff_path}")
-    # print(f"Shape of the organized point cloud: {organized_pc.shape}")
-    # print(f"Data type of the organized point cloud: {organized_pc.dtype}")
     rgb_path = str(tiff_path).replace("xyz", "rgb").replace("tiff", "png")
+    # print(rgb_path)
     gt_path = str(tiff_path).replace("xyz", "gt").replace("tiff", "png")
-    # print(f"Saving RGB image to {rgb_path}")
-    # print(f"Saving ground truth image to {gt_path}")
-    organized_rgb=np.array(Image.open(rgb_path))
-    print(f"Shape of the organized RGB image: {organized_rgb.shape}")
+    # print(gt_path)
+    organized_rgb = np.array(Image.open(rgb_path))
+    organized_gt=None
+    gt_exists = os.path.isfile(gt_path)
+    if gt_exists:
+        organized_gt = np.array(Image.open(gt_path))
+        # print(organized_gt)
+    
+    planeless_organized_pc, planeless_organized_rgb = remove_plane(organized_pc, organized_rgb)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
