64729c75dbc9703d55ab003993152ee0aedea96a
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-28 11:09:02 +0800

    可以看出预处理的方法

---
+++ M3DM-from-Scratch/utils/mvtec3d_util.py
@@ -0,0 +1,18 @@
+
+'''
+Author: wangwei83 wangwei83@cuit.edu.cn
+Date: 2024-05-28 11:05:44
+LastEditors: wangwei83 wangwei83@cuit.edu.cn
+LastEditTime: 2024-05-28 11:06:42
+FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/mvtec3d_util.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
+import tifffile as tiff
+
+def read_tiff_organized_pc(tiff_path):
+    '''
+    Read a tiff file and return the organized point cloud
+    '''
+    organized_pc = tiff.imread(tiff_path)
+    return organized_pc
--- M3DM-from-Scratch/utils/preprocessing.py
+++ M3DM-from-Scratch/utils/preprocessing.py
@@ -2,19 +2,28 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-28 10:48:48
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-28 10:56:06
+LastEditTime: 2024-05-28 11:08:14
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/preprocessing.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 import argparse
 from pathlib import Path
+import mvtec3d_util as mvt_util
 
+def preprocess_pc(tiff_path):
+    organized_pc = mvt_util.read_tiff_organized_pc(tiff_path)
+    print(f"Preprocessing {tiff_path}")
+    print(f"Shape of the organized point cloud: {organized_pc.shape}")
+    print(f"Data type of the organized point cloud: {organized_pc.dtype}")
 
-if __name__ == '__main__':    # Your code here
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dataset_path', type=str, help='path to the dataset')
     args=parser.parse_args()
     root_path = args.dataset_path
-    path=Path(root_path)
-    print(path)
+    paths=Path(root_path).rglob('*.tiff')
+    print(f"Found {len(list(paths))} tiff files in {root_path}")
+    
+    for path in Path(root_path).rglob('*.tiff'):
+        preprocess_pc(path)
