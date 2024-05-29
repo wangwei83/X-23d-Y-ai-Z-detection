46954ba8bd706eb6530d50c64573232250e6eafa
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-29 16:30:28 +0800

    增加一个tiff文件转化为txt的函数实现

--- M3DM-from-Scratch/utils/preprocessing.py
+++ M3DM-from-Scratch/utils/preprocessing.py
@@ -2,18 +2,20 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-28 10:48:48
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-29 15:03:58
+LastEditTime: 2024-05-29 16:24:44
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/preprocessing.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 import argparse
+import math
 import os
 from pathlib import Path
 from PIL import Image
 import numpy as np
 import mvtec3d_util as mvt_util
 import open3d as o3d
+
 
 def get_edges_of_pc(organized_pc):
     # print(organized_pc)
@@ -59,6 +61,32 @@ def remove_plane(organized_pc_clean, organized_rgb,distance_threshold=0.005):
     
     return clean_planeless_organized_pc, planeless_organized_rgb
 
+def roundup_next_100(x):
+    # 将输入的x向上取整到最近的100的倍数
+    return int(math.ceil(x/100.0))*100
+def connected_components_cleaning(organized_pc,organized_rgb,image_path):    
+    unorganized_pc = mvt_util.organized_pc_to_unorganized_pc(organized_pc)
+    unorganized_rgb = mvt_util.organized_pc_to_unorganized_pc(organized_rgb)
+    
+    nonzero_indices = np.nonzero(np.all(unorganized_pc!=0,axis=1))[0]
+    
+def pad_cropped_pc(cropped_pc, single_channel=False):
+    orig_h,orig_w = cropped_pc.shape[0],cropped_pc.shape[1]
+    round_orig_h=roundup_next_100(orig_h)
+    round_orig_w = roundup_next_100(orig_w)
+    large_side = max(round_orig_h,round_orig_w)
+    
+    a=(large_side-orig_h)//2
+    aa = large_side-orig_h-a
+    
+    b=(large_side-orig_w)//2
+    bb = large_side-orig_w-b
+    
+    if single_channel:
+        return np.pad(cropped_pc,pad_width=((a,aa),(b,bb)),mode='constant')
+    else:
+        return np.pad(cropped_pc,pad_width=((a,aa),(b,bb),(0,0)),mode='constant') 
+
 def preprocess_pc(tiff_path):
     organized_pc = mvt_util.read_tiff_organized_pc(tiff_path)
     rgb_path = str(tiff_path).replace("xyz", "rgb").replace("tiff", "png")
@@ -71,8 +99,35 @@ def preprocess_pc(tiff_path):
     if gt_exists:
         organized_gt = np.array(Image.open(gt_path))
         
-    # REMOVE PLANE
+    # REMOVE PLANE，去除平面的规则点云和RGB图像
     planeless_organized_pc, planeless_organized_rgb = remove_plane(organized_pc, organized_rgb)
+
+    # PAD WITH ZEROS TO LARGEST SIDE
+    # (so that the final image is square)
+    padded_planeless_organized_pc = pad_cropped_pc(planeless_organized_pc,single_channel=False)
+    padded_planeless_organized_rgb = pad_cropped_pc(planeless_organized_rgb,single_channel=False)
+    if gt_exists:
+        padded_planeless_organized_gt = pad_cropped_pc(organized_gt,single_channel=True)
+    # 点云连通组件分析和清理工作，跟聚类是不是有什么关系
+    # organized_clustered_pc,organized_clustered_rgb=connected_components_cleaning(padded_planeless_organized_pc,padded_planeless_organized_rgb,tiff_path)
+
+
+
+def tiff_to_pointcloud(path):
+    
+    # 获取文件名（不包括扩展名）
+    base_name = os.path.splitext(os.path.basename(path))[0]
+    # 创建输出文件的路径
+    output_path = base_name + '.txt'
+
+    # 读取tiff文件
+    rgb_path = str(path).replace("xyz", "rgb").replace("tiff", "png")  
+    organized_rgb = np.array(Image.open(rgb_path))
+    
+    # 将3D数组转换为2D数组
+    organized_rgb_2d = organized_rgb.reshape(-1, organized_rgb.shape[-1])
+    # 保存2D数组
+    np.savetxt("../../"+output_path, organized_rgb_2d, fmt='%d')
 
 
 if __name__ == '__main__':
@@ -89,7 +144,11 @@ if __name__ == '__main__':
     
     # 测试分支，减少文件的读取次数
     for i, path in enumerate(Path(root_path).rglob('*.tiff')):
-        # print(i)
+        print(i)
+        print(path)
+        # 这个函数的主要功能是将 TIFF 图像文件转换为点云数据
+        tiff_to_pointcloud(path)
+
         preprocess_pc(path)
         if i >= 5:
             break
