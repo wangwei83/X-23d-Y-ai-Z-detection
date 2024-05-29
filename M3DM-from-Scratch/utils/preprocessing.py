f0cf92bcf09fa22ef91d9f2158c8b7955423c85c
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-29 15:03:14 +0800

    提交

--- M3DM-from-Scratch/utils/preprocessing.py
+++ M3DM-from-Scratch/utils/preprocessing.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-28 10:48:48
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-29 14:04:52
+LastEditTime: 2024-05-29 14:44:57
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/preprocessing.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -70,8 +70,7 @@ def preprocess_pc(tiff_path):
     gt_exists = os.path.isfile(gt_path)
     if gt_exists:
         organized_gt = np.array(Image.open(gt_path))
-        # print(organized_gt)
-    
+        
     # REMOVE PLANE
     planeless_organized_pc, planeless_organized_rgb = remove_plane(organized_pc, organized_rgb)
 
