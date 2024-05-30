8134c48a931bf694652f853aecf8260a95cc0be7
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-30 22:57:06 +0800

    优化格式

--- M3DM-from-Scratch/m3dm_runner.py
+++ M3DM-from-Scratch/m3dm_runner.py
@@ -3,7 +3,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-27 21:03:16
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-30 19:45:31
+LastEditTime: 2024-05-30 22:52:04
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/m3dm_runner.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -32,13 +32,9 @@ class M3DM():
         
         flag =0
         for sample, _ in tqdm(train_loader, desc='Processing samples'):
-            # if sample == self.count:  # Replace "i" with "sample"
-            #     break
-            # print("Image shape: ", img.shape)
-            # print("Mask shape: ", mask.shape)
-            
-            # self.method_name['DINO+Point_MAE'].add_sample_to_mem_bank(img, class_name)
-            # print(self.methods.values())
             for method in self.methods.values():
+                # print(method)
+                # print(sample)
+                print(class_name)
                 method.add_sample_to_mem_bank(sample, class_name)
             flag = 1
