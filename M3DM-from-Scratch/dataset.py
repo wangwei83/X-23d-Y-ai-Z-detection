cff5d93ecfdeb42224341b0f1d7158c5d95d7f4a
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-30 19:53:02 +0800

    可以训练，但是不完整

--- M3DM-from-Scratch/dataset.py
+++ M3DM-from-Scratch/dataset.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-27 19:43:50
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-30 18:12:02
+LastEditTime: 2024-05-30 19:41:16
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/dataset.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -11,6 +11,9 @@ import glob
 import os
 import numpy as np
 from torch.utils.data import DataLoader
+from PIL import Image
+from utils.mvtec3d_util import *
+from torchvision import transforms
 
 def mvtec3d_classes():
     return ['bagel', 
@@ -23,12 +26,23 @@ def mvtec3d_classes():
             'potato', 
             'rope', 
             'tire']
+RGB_SIZE = 224
+
 class BaseAnomalyDetectionDataset:
     def __init__(self, split, class_name, img_size, dataset_path='datasets/eyecandies_preprocessed'):
-        self.cls = class_name
-        self.img_size = img_size
+        # self.cls = class_name
+        # self.img_size = img_size
+        # self.img_path = os.path.join(dataset_path, self.cls, split)
+        self.IMAGENET_MEAN = [0.485, 0.456, 0.406]
+        self.IMAGENET_STD = [0.229, 0.224, 0.225]
+        self.cls=   class_name
+        self.size = img_size
         self.img_path = os.path.join(dataset_path, self.cls, split)
-        
+        self.rgb_transform = transforms.Compose([
+            transforms.Resize((RGB_SIZE, RGB_SIZE),interpolation=transforms.InterpolationMode.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=self.IMAGENET_MEAN, std=self.IMAGENET_STD)
+        ])        
         
 class TrainDataset(BaseAnomalyDetectionDataset):
     def __init__(self, class_name, img_size, dataset_path='datasets/eyecandies_preprocessed'):
@@ -36,27 +50,42 @@ class TrainDataset(BaseAnomalyDetectionDataset):
         super().__init__(split='train', class_name=class_name, img_size=img_size, dataset_path=dataset_path)
         self.img_paths,self.labels = self.load_dataset()
         
+    def load_dataset(self):
+        img_tot_paths = []  
+        tot_labels = []  
+        rgb_paths = glob.glob(os.path.join(self.img_path, 'good','rgb')+"/*.png")
+        tiff_paths = glob.glob(os.path.join(self.img_path, 'good','xyz')+"/*.tiff")
+        rgb_paths.sort()
+        tiff_paths.sort()
+        # 配对
+        sample_paths = list(zip(rgb_paths, tiff_paths))
+        img_tot_paths.extend(sample_paths)  
+        tot_labels.extend([0]*len(sample_paths))
+            
+        return img_tot_paths, tot_labels
+    
+    def __len__(self):
+        return len(self.img_paths)
+      
     def __getitem__(self, idx):
         img_path,label=self.img_paths[idx],self.labels[idx]
         rgb_path = img_path[0]
         tiff_path = img_path[1]
+        img = Image.open(rgb_path).convert('RGB')
         
-        return rgb_path, tiff_path, label
+        img=self.rgb_transform(img)
+        organized_pc = read_tiff_organized_pc(tiff_path)
+
+        depth_map_3channel = np.repeat(organized_pc_to_depth_map(organized_pc)[:,:,np.newaxis], 3, axis=2)     
+        resized_depth_map_3channel = resize_organized_pc(depth_map_3channel)
+        resized_organized_pc = resize_organized_pc(organized_pc, target_height=self.size, target_width=self.size)
+        resized_organized_pc =resized_organized_pc.clone().detach().float()    
+                 
+        return (img,resized_organized_pc,resized_depth_map_3channel), label
         
-    def __len__(self):
-        return len(self.img_paths)
     
-    def load_dataset(self):
-        img_paths = []  # Change variable name from "img_tot_paths" to "img_paths"
-        labels = []  # Change variable name from "tot_labels" to "labels"
-        rgb_paths = glob.glob(os.path.join(self.img_path, 'good','rgb')+"/*.png")
-        tiff_paths = glob.glob(os.path.join(self.img_path, 'good','xyz')+"/*.tiff")
-        for img_name in os.listdir(self.img_path):
-            img_path = os.path.join(self.img_path, img_name)
-            img_paths.append(img_path)
-            labels.append(1)
-            
-        return img_paths, labels
+    
+    
        
         
 def get_data_loader(split, class_name, img_size, args):
--- M3DM-from-Scratch/utils/mvtec3d_util.py
+++ M3DM-from-Scratch/utils/mvtec3d_util.py
@@ -3,12 +3,13 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-28 11:05:44
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-28 23:27:38
+LastEditTime: 2024-05-30 19:39:46
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/utils/mvtec3d_util.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 import tifffile as tiff
+import torch
 
 def organized_pc_to_unorganized_pc(organized_pc):
     '''
@@ -24,3 +25,22 @@ def read_tiff_organized_pc(tiff_path):
     '''
     organized_pc = tiff.imread(tiff_path)
     return organized_pc
+def resize_organized_pc(organized_pc, target_height=224, target_width=224,tensor_out=True):
+    # 这行代码将 organized_pc 转换为一个 PyTorch 张量，并对其进行了一系列的变换，包括重新排列维度、添加新的维度和确保内存布局连续
+    torch_organized_pc = torch.tensor(organized_pc).permute(2,0,1).unsqueeze(dim=0).contiguous()
+    
+    # 这行代码使用 PyTorch 的 interpolate 函数对一个名为 torch_organized_pc 的张量进行了尺寸调整（或称为插值）
+    torch_resized_organized_pc = torch.nn.functional.interpolate(torch_organized_pc, size=(target_height, target_width),
+                                                                 mode='nearest')
+    if tensor_out:
+        return torch_resized_organized_pc.squeeze(dim=0).contiguous()
+    else:
+        return torch_resized_organized_pc.squeeze().permute(1,2,0).contiguous().numpy()
+    
+    # return organized_pc[::organized_pc.shape[0]//target_height, ::organized_pc.shape[1]//target_width]
+    
+def organized_pc_to_depth_map(organized_pc):
+    '''
+    Convert organized point cloud to depth map
+    '''
+    return organized_pc[:,:,2]
