1b010161f0e6169ccbe34e77d09fa70f9f086666
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-31 21:54:11 +0800

    有点问题

--- misc-test-windows/timm-test.py
+++ misc-test-windows/timm-test.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-31 21:42:00
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-31 21:42:15
+LastEditTime: 2024-05-31 21:53:13
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/misc-test-windows/timm-test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
