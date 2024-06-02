2fe4c7cf11061258416ac0496813742165ee68ed
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-02 18:25:51 +0800

    想办法只有timm.create

--- M3DM-from-Scratch/models/models.py
+++ M3DM-from-Scratch/models/models.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-31 00:01:07
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-05-31 11:32:50
+LastEditTime: 2024-06-01 23:09:05
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/M3DM-from-Scratch/models/models.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -21,7 +21,7 @@ class Model(torch.nn.Module):
         kwargs={'features_only':True if out_indices is None else False}
         if out_indices:
             kwargs.update({'out_indices':out_indices})
-            
+        print(timm.create_model(model_name=rgb_backbone_name,pretrained=True,checkpoint_path=checkpoint_path,**kwargs)) 
         self.rgb_backbone = timm.create_model(model_name=rgb_backbone_name,pretrained=True,checkpoint_path=checkpoint_path,**kwargs)
        
         
--- misc-test-windows/timm-install-test.py
+++ misc-test-windows/timm-install-test.py
@@ -2,36 +2,36 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-31 23:45:25
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-01 22:29:51
+LastEditTime: 2024-06-01 23:05:54
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/misc-test-windows/timm-install-test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
-from transformers import AutoImageProcessor, ResNetForImageClassification
-import torch
-from datasets import load_dataset
-from PIL import Image
-import numpy as np
+# from transformers import AutoImageProcessor, ResNetForImageClassification
+# import torch
+# from datasets import load_dataset
+# from PIL import Image
+# import numpy as np
 
-# dataset = load_dataset("huggingface/cats-image")
-# image = dataset["test"]["image"][0]
+# # dataset = load_dataset("huggingface/cats-image")
+# # image = dataset["test"]["image"][0]
 
-# 读取图像
-image = Image.open("cats_image.jpeg")
-# 将 PIL 图像转换为 NumPy 数组
-image = np.array(image)
+# # 读取图像
+# image = Image.open("cats_image.jpeg")
+# # 将 PIL 图像转换为 NumPy 数组
+# image = np.array(image)
 
 
-processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
-model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
+# processor = AutoImageProcessor.from_pretrained("microsoft/resnet-50")
+# model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
 
-inputs = processor(image, return_tensors="pt")
+# inputs = processor(image, return_tensors="pt")
 
-with torch.no_grad():
-    logits = model(**inputs).logits
+# with torch.no_grad():
+#     logits = model(**inputs).logits
 
-# model predicts one of the 1000 ImageNet classes
-predicted_label = logits.argmax(-1).item()
-print(model.config.id2label[predicted_label])
+# # model predicts one of the 1000 ImageNet classes
+# predicted_label = logits.argmax(-1).item()
+# print(model.config.id2label[predicted_label])
 
 # (M3DM) (base) c206@c206linux:/data/wangwei/X-23d-Y-ai-Z-detection/misc-test-windows$ python timm-install-test.py 
 # Traceback (most recent call last):
@@ -123,3 +123,5 @@ print(model.config.id2label[predicted_label])
 # OSError: We couldn't connect to 'https://huggingface.co' to load this file, couldn't find it in the cached files and it looks like microsoft/resnet-50 is not the path to a directory containing a file named preprocessor_config.json.
 # Checkout your internet connection or see how to run the library in offline mode at 'https://huggingface.co/docs/transformers/installation#offline-mode'.
 # (M3DM) (base) c206@c206linux:/data/wangwei/X-23d-Y-ai-Z-detection/misc-test-windows$ 
+import timm
+print(timm.models.create_model('tf_efficientnetv2_s').default_cfg)
