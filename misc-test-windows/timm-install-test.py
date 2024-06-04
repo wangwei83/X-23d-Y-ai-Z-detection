dd048458c747b0878dd7346f7ac40cc38abe8dc9
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-04 21:53:18 +0800

    测试

--- misc-test-windows/timm-install-test.py
+++ misc-test-windows/timm-install-test.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-31 23:45:25
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-02 21:54:09
+LastEditTime: 2024-06-03 22:43:39
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/misc-test-windows/timm-install-test.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
--- misc-test-windows/timm_resnet.py
+++ misc-test-windows/timm_resnet.py
@@ -2,17 +2,32 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-06-02 22:06:32
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-03 18:51:41
+LastEditTime: 2024-06-04 21:53:09
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/misc-test-windows/timm_resnet.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 import timm
-avail_pretrained_models = timm.list_models(pretrained=True)
-len(avail_pretrained_models)
-print(len(avail_pretrained_models))
+from timm.models.efficientnet import _cfg
+import torch
 
-all_densnet_models = timm.list_models("*densenet*")
-print(all_densnet_models)
 
-model = timm.create_model('resnet34',num_classes=10,pretrained=True)
-print(model.default_cfg)
+# avail_pretrained_models = timm.list_models(pretrained=True)
+# len(avail_pretrained_models)
+# print(len(avail_pretrained_models))
+
+# all_densnet_models = timm.list_models("*densenet*")
+# print(all_densnet_models)
+
+config = _cfg(url='', file='/data/wangwei/resnetv2_152x2_bit.bin') #file为本地文件路径
+
+model = timm.create_model(
+                                "resnet34",
+                                pretrained=True,
+                                features_only=True,
+                                pretrained_cfg=config
+                            )
+
+x = torch.randn(1,3,224,224)
+output = model(x)
+output.shape
+# 测试
