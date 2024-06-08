287a71b961f7453efa5c7d5d28ba8e578b4f185c
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-08 11:28:14 +0800

    提交

--- misc-test-windows/vit-base-patch16-224.py
+++ misc-test-windows/vit-base-patch16-224.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-06-05 23:14:20
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-05 23:20:50
+LastEditTime: 2024-06-08 10:34:31
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/misc-test-windows/vit-base-patch16-224.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -16,4 +16,5 @@ import requests
 url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
 image = Image.open(requests.get(url, stream=True).raw)
 
-processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+# processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224')
+processor = ViTImageProcessor.from_pretrained('/data/wangwei/vit-base-patch16-224-pytorch_model.bin')
