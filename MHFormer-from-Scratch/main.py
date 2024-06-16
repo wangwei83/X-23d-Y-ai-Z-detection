7df63665bc1f3479ba5e35c9718606f5e9e0a6f1
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-16 20:02:00 +0800

    Hourglass Tokenizer for Efficient Transformer-Based 3D Human Pose Estimation

--- MHFormer-from-Scratch/main.py
+++ MHFormer-from-Scratch/main.py
@@ -2,10 +2,13 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-30 20:34:37
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-16 19:58:06
+LastEditTime: 2024-06-16 20:01:49
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/MHFormer-from-Scratch/main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 # https://github.com/Vegetebird/MHFormer
 # MHFormer: Multi-Hypothesis Transformer for 3D Human Pose Estimation
+
+# https://github.com/NationalGAILab/HoT
+# Hourglass Tokenizer for Efficient Transformer-Based 3D Human Pose Estimation
