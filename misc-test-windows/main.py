4a26ddd5d63a23df765b3994ce64d3c395ddacbc
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-16 19:50:56 +0800

    提交修改

--- misc-test-windows/main.py
+++ misc-test-windows/main.py
@@ -1,10 +1,17 @@
+'''
+Author: wangwei83 wangwei83@cuit.edu.cn
+Date: 2024-05-31 10:10:31
+LastEditors: wangwei83 wangwei83@cuit.edu.cn
+LastEditTime: 2024-06-16 19:50:43
+FilePath: /wangwei/X-23d-Y-ai-Z-detection/misc-test-windows/main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import cv2
 import numpy as np
 
 # 加载图像
 img = cv2.imread('X-23d-Y-ai-Z-detection/image/人工智能点云处理机深度学习算法.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 # Canny 边缘检测
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
