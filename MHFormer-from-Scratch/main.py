c6a1c80164e9d08cefdef992444371d51f5734c8
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-16 20:09:18 +0800

    🔥HoT🔥 is tested on Ubuntu 18 with Pytorch 1.7.1 and Python 3.9.

--- MHFormer-from-Scratch/main.py
+++ MHFormer-from-Scratch/main.py
@@ -2,7 +2,7 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-30 20:34:37
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-16 20:01:49
+LastEditTime: 2024-06-16 20:09:13
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/MHFormer-from-Scratch/main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -12,3 +12,4 @@ Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查�
 
 # https://github.com/NationalGAILab/HoT
 # Hourglass Tokenizer for Efficient Transformer-Based 3D Human Pose Estimation
+# 🔥HoT🔥 is tested on Ubuntu 18 with Pytorch 1.7.1 and Python 3.9.
