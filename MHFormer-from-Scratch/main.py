d72911a137187bbc613220d4fcb1db64efb6af70
wangwei83 <wangwei83@cuit.edu.cn>
2024-06-16 20:10:32 +0800

    (CVPR), 2024

--- MHFormer-from-Scratch/main.py
+++ MHFormer-from-Scratch/main.py
@@ -2,14 +2,14 @@
 Author: wangwei83 wangwei83@cuit.edu.cn
 Date: 2024-05-30 20:34:37
 LastEditors: wangwei83 wangwei83@cuit.edu.cn
-LastEditTime: 2024-06-16 20:09:13
+LastEditTime: 2024-06-16 20:10:27
 FilePath: /wangwei/X-23d-Y-ai-Z-detection/MHFormer-from-Scratch/main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
 
 # https://github.com/Vegetebird/MHFormer
-# MHFormer: Multi-Hypothesis Transformer for 3D Human Pose Estimation
+# MHFormer: Multi-Hypothesis Transformer for 3D Human Pose Estimation  (CVPR), 2022
 
 # https://github.com/NationalGAILab/HoT
-# Hourglass Tokenizer for Efficient Transformer-Based 3D Human Pose Estimation
+# Hourglass Tokenizer for Efficient Transformer-Based 3D Human Pose Estimation  (CVPR), 2024
 # 🔥HoT🔥 is tested on Ubuntu 18 with Pytorch 1.7.1 and Python 3.9.
